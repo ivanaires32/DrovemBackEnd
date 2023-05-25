320fbc9239d05cd92188648db1d852fdca56d700
Ivan <ivanaires32@gmail.com>
2023-05-25 17:03:38 -0300

    feat: info aluno

--- src/Controllers/turmas.controllers.js
+++ src/Controllers/turmas.controllers.js
@@ -1,15 +1,29 @@
 import { db } from "../Database/dataBase.js"
 
 export async function getTurmas(req, res) {
-    const { id } = req.params
+    const { id_turma } = req.params
     try {
         //autenticar
         const turmas = await db.query(`
             SELECT * FROM alunos
             WHERE id_turma=$1
-        ;`, [id])
+        ;`, [id_turma])
 
         res.status(200).send(turmas.rows)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function getAluno(req, res) {
+    const { id } = req.params
+    try {
+        const aluno = await db.query(`
+            SELECT * FROM alunos
+            WHERE id_aluno=$1
+        ;`, [id])
+
+        res.status(200).send(aluno.rows[0])
     } catch (err) {
         res.status(500).send(err.message)
     }
--- src/Routes/turmas.routes.js
+++ src/Routes/turmas.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getTurmas } from "../Controllers/turmas.controllers.js";
+import { getAluno, getTurmas } from "../Controllers/turmas.controllers.js";
 
 const turmaRouter = Router()
 
-turmaRouter.get("/turmas/:id", getTurmas)
+turmaRouter.get("/turmas/:id_turma", getTurmas)
+turmaRouter.get("/turmas/:id_turma/:id", getAluno)
 
 export default turmaRouter
